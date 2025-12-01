c6c1668e17820be45107a48bbd81772d71089dd9
THAMER CHAOUECH <thamerc@isasbs.com>
2025-12-01 10:29:44 +0100

    password

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,17 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // The plugin adds .decrypt-success when content is decrypted
     const observer = new MutationObserver(() => {
-        const decrypted = document.querySelector(".decrypt-success");
-        if (decrypted) {
-            const sidebar = document.querySelector(".md-sidebar");
-            if (sidebar) {
-                sidebar.style.display = "block";
-            }
+        // When content is decrypted
+        if (document.querySelector(".decrypt-success")) {
+            // Show all sidebars
+            document.querySelectorAll(
+                ".md-sidebar.md-sidebar--primary, .md-sidebar--primary, [data-md-component='sidebar'][data-md-type='navigation'], .md-sidebar.md-sidebar--secondary, .md-sidebar--secondary, [data-md-component='sidebar'][data-md-type='toc']"
+            ).forEach(sb => sb.classList.add("show-sidebars"));
         }
     });
 
     observer.observe(document.body, {
-        subtree: true,
-        attributes: true
+        childList: true,
+        subtree: true
     });
 });
